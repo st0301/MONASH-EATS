70a579ea2163cbd53b891065c417559b270b66fd
Saken Tleuberdin <sakentleuberdin@gmail.com>
2019-08-18 12:41:24 +1000

    Create restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,46 @@
+
+class Restaurant(): #Function to set restaurant details
+
+    def __init__(self):
+        self.rest_id = None
+        self.name = None
+        self.category = None
+        self.phone_number = None
+        self.address = None
+        self.suburb = None
+        self.postcode = None
+
+    def set_rest_id(self,rest_id):#Function to set the restaurant id
+        pass
+    def get_rest_id(self): #Function to get the restaurant id
+        pass
+    def set_name(self,name):#Function to set the restaurant name
+        pass
+    def get_name(self): #Function to get the restaurant name
+        pass
+    def set_category(self,category): #Function to set the restaurant cuisine type
+        pass
+    def get_category(self): #Function to get the restaurant cuisine type
+        pass
+    def set_phone_number(self,phone_number): #Function to set the restaurant phone number
+        pass
+    def get_phone_number(self): #Function to get the restaurantphone number
+        pass
+    def set_address(self,address): #Function to set the restaurant address
+        pass
+    def get_address(self): #Function to set the restaurant address
+        pass
+    def set_suburb(self,suburb): #Function to set the restaurant suburb location
+        pass
+    def get_suburb(self):#Function to get the restaurant suburb location
+        pass
+    def set_postcode(self,postcode): #Function to set the restaurant pincode
+        pass
+    def get_postcode(self): #Function to get the restaurant pincode
+        pass
+
+
+
+
+
+
