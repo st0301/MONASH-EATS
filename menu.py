eab175a413a3ea821a04794925e36924f47b8350
Saken Tleuberdin <sakentleuberdin@gmail.com>
2019-08-18 12:40:33 +1000

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,12 @@
+
+class Menu(): #Function to display and write the category
+
+
+    def __init__(self):
+        self.category = None
+
+    def set_category(self,category): #Function to write the category
+        pass
+
+    def get_category(self): #Function to get the category
+        pass
