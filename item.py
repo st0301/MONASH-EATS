2e01415935e76fe68a4ebfdeb30e2847c117a0fb
Saken Tleuberdin <sakentleuberdin@gmail.com>
2019-08-18 12:40:17 +1000

    Create item.py

---
+++ item.py
@@ -0,0 +1,24 @@
+class Item(): #Class to set item id and detals
+
+    def __init__(self):
+        self.id_item = None
+        self.name = None
+        self.description = None
+        self.price = None
+
+    def set_id_item(self,id_item): #Set Item IDto the item name
+        pass
+    def get_id_item(self):#Fetch Item ID
+        pass
+    def set_name(self,name):#Fuction to set item name
+        pass
+    def get_name(self):#Function to get the name of the item
+        pass
+    def set_description(self,description):#function to write the description of the item
+        pass
+    def get_description(self):#Functin to get the description of the item
+        pass
+    def set_price(self,price):#Function to set the price of the item
+        pass
+    def get_price(self):#Function to get the proce of the item
+        pass
