1425b65243197c9ef0cf2ffb6b03fd7258f761fa
Saken Tleuberdin <sakentleuberdin@gmail.com>
2019-08-18 12:41:40 +1000

    Create user.py

---
+++ user.py
@@ -0,0 +1,55 @@
+
+
+class User(): #Class user to get  details of the user
+
+    def __init__(self):
+        self.first_name = None
+        self.last_name = None
+        self.email = None
+        self.home_address = None
+        self.suburb = None
+        self.postcode = None
+        self.phone_number = None
+        self.password = None
+
+    def set_first_name(self,first_name):#Function to set the user first name
+        pass
+
+    def get_first_name(self): #Function to get the user first name
+        pass
+
+    def set_last_name(self, last_name): #Function to set the user last name
+        pass
+
+    def get_last_name(self): #Function to get the user last name
+        pass
+    def set_email(self,email): #Function to set the user email id
+        pass
+    def get_email(self):#Function to get the user email id
+        pass
+    def set_home_address(self,home_address):#Function to set the user address
+        pass
+    def get_home_address(self):#Function to get the user address
+        pass
+    def set_suburb(self,suburb): #Function to set the user address
+        pass
+    def get_suburb(self): #Function to get the user address
+        pass
+    def set_postcode(self,postcode):#Function to set the user postcode
+        pass
+    def get_postcode(self):#Function to get the user postcode
+        pass
+    def set_phone_number(self,phone_number): #Function to set the user phone number
+        pass
+    def get_phone_number(self): #Function to get the user phone number
+        pass
+    def set_password(self,password): #Function to set the user password
+        pass
+    def get_password(self): #Function to get the user password
+        pass
+
+
+
+
+
+
