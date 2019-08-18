2134c10c649196c0e844fb5d61d46fdd1e28553c
Saken Tleuberdin <sakentleuberdin@gmail.com>
2019-08-18 12:40:49 +1000

    Create order.py

---
+++ order.py
@@ -0,0 +1,7 @@
+class Order(): #Function to write the order status
+    def __init__(self):
+        self.__order = None
+    def set_status(self,status): #Function to set the status of the order
+        pass
+    def get_status(self):  #Function to get the status of the order
+        pass
