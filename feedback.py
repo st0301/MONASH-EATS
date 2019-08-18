3cf33ad45c73c41fb94c92053ac879ed111de236
Saken Tleuberdin <sakentleuberdin@gmail.com>
2019-08-18 12:40:02 +1000

    Create feedback.py

---
+++ feedback.py
@@ -0,0 +1,10 @@
+
+class Feedback():#Class to ask feedback to customer on order.
+
+    def set_question(self,question):
+        pass
+    def get_question(self):
+        pass
+
+    def rate(self):
+        pass
