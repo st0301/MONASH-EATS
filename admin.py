536e176e8d0c43ab905004134d8e86c1f0887c75
Saken Tleuberdin <sakentleuberdin@gmail.com>
2019-08-18 12:39:17 +1000

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,6 @@
+class Admin(): #Admin class to fetch the admin data to delete account and restaurant
+
+    def __delete_account(self,account):
+        pass
+    def __delete_restaurant(self,restaurant):
+        pass
